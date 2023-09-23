0d8ab69b07da374d595c493590b43e0659c9ee92
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2023-09-23 17:14:04 +0100

    feat: fixes

--- src/components/register/desktop/SuccessCheckImg.tsx
+++ src/components/register/desktop/SuccessCheckImg.tsx
@@ -2,7 +2,7 @@ import Image from "../../../assets/images/successfully-done.png";
 
 const SuccessCheckImg = () => {
   return (
-    <div className="h-[174px] max-w-[174px] w-full md:max-w-[290px] md:h-[290px] w-full overflow-hidden absolute bottom-[20px] left-0">
+    <div className="h-[174px] max-w-[174px]  md:max-w-[290px] md:h-[290px] w-full overflow-hidden absolute bottom-[20px] left-0">
       <img
         src={Image}
         alt="success check image"
